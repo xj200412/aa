d9021ec9a200fd43a9342ff2f72b2dd68dc2fe0a
xj200412 <726419144@qq.com>
2025-08-29 22:50:04 +0800

    想

---
+++ Campus_Management_System/src/main/java/demo/campus_management_system/entity/VO/ClassroomUsageResultVO.java
@@ -0,0 +1,6 @@
+package demo.campus_management_system.entity.VO;
+public class ClassroomUsageResultVO {
+    private StatisticsVO statistics; // 统计数据单独封装
+    private List<ClassroomUsageVO> records;
+    // getter/setter
+}
