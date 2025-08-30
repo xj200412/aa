1a354ad6df5d789fafc76343257b9c68c8ae2bcd
xj200412 <726419144@qq.com>
2025-08-30 12:49:25 +0800

    重

--- Campus_Management_System/src/main/java/demo/campus_management_system/service/service_interface/ClassroomManagerService.java
+++ Campus_Management_System/src/main/java/demo/campus_management_system/service/service_interface/ClassroomManagerService.java
@@ -2,8 +2,8 @@ package demo.campus_management_system.service.service_interface;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import demo.campus_management_system.entity.Classroom_manager;
-import demo.campus_management_system.entity.DTO.ClassroomManageQueryDTO;
 import demo.campus_management_system.entity.DTO.BatchUpdateStatusDTO;
+import demo.campus_management_system.entity.DTO.ClassroomManageQueryDTO;
 import demo.campus_management_system.entity.VO.ClassroomManageVO;
 import demo.campus_management_system.entity.VO.ApplyInfoManageVO;
 import demo.campus_management_system.entity.DTO.ResultDTO;
@@ -24,4 +24,5 @@ public interface ClassroomManagerService extends IService<Classroom_manager> {
      * 批量更新教室状态
      */
     ResultDTO<Boolean> batchUpdateStatus(String token, BatchUpdateStatusDTO updateDTO);
+
 }
